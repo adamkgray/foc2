41bc58fe2b1b0331e584cecec368f2a5f54f2f84
Adam K Gray <adamkgray@pm.me>
2022-02-10 17:23:35 +0000

    WIP

--- tree/main.go
+++ tree/main.go
@@ -3,19 +3,72 @@ package main
 import "fmt"
 
 func main() {
-	t2l := &Tree{info: "t2l"}
-	t2r := &Tree{info: "t2r"}
-	t := &Tree{info: "root", llink: t2l, rlink: t2r}
+	// REGULAR TREES
+	t0 := &Tree{info: "t0"}
+	t1 := &Tree{info: "t1"}
+	t2 := &Tree{info: "t2"}
+	t3 := &Tree{info: "t3"}
+	t4 := &Tree{info: "t4"}
+	t5 := &Tree{info: "t5"}
+
+	// regular links
+
+	t0.llink = t1
+	t0.rlink = t2
+	t1.llink = t3
+	t1.rlink = t4
+	t4.llink = t5
 
 	fmt.Println("pre-order:")
-	PreOrder(t)
-	fmt.Print("\n")
+	PreOrder(t0)
+	fmt.Print("\n\n")
 
 	fmt.Println("in-order:")
-	InOrder(t)
-	fmt.Print("\n")
+	InOrder(t0)
+	fmt.Print("\n\n")
 
 	fmt.Println("post-order:")
-	PostOrder(t)
-	fmt.Print("\n")
+	PostOrder(t0)
+	fmt.Print("\n\n")
+
+	// THREADED TREES
+
+	tthead := &ThreadedTree{info: "h"}
+	tt0 := &ThreadedTree{info: "t0"}
+	tt1 := &ThreadedTree{info: "t1"}
+	tt2 := &ThreadedTree{info: "t2"}
+	tt3 := &ThreadedTree{info: "t3"}
+	tt4 := &ThreadedTree{info: "t4"}
+	tt5 := &ThreadedTree{info: "t5"}
+
+	// definition of head
+	tthead.rtag = false
+	tthead.rlink = tthead
+
+	// regular links
+	tthead.llink = tt0
+	tt0.llink = tt1
+	tt0.rlink = tt2
+
+	tt1.llink = tt3
+	tt1.rlink = tt4
+
+	tt4.llink = tt5
+
+	// threaded links
+	tt3.rtag = true
+	tt3.rlink = tt1
+
+	tt5.rtag = true
+	tt5.rlink = tt4
+
+	tt4.rtag = true
+	tt4.rlink = tt0
+
+	tt2.rtag = true
+	tt2.rlink = tthead
+
+	fmt.Println("threaded traversal:")
+	ThreadedTraversal(tthead)
+	fmt.Print("\n\n")
 }
--- tree/tree.go
+++ tree/tree.go
@@ -8,6 +8,14 @@ type Tree struct {
 	rlink *Tree
 }
 
+type ThreadedTree struct {
+	llink *ThreadedTree
+	//ltag  bool // (threaded only) denotes if left pointer points to predecessor (in-order)
+	info  string
+	rlink *ThreadedTree
+	rtag  bool // (threaded only) denotes if right pointer points to successor (in-order)
+}
+
 func PreOrder(t *Tree) {
 	if t == nil {
 		return
@@ -33,4 +41,49 @@ func PostOrder(t *Tree) {
 	PostOrder(t.llink)
 	PostOrder(t.rlink)
 	fmt.Printf("%s ", t.info)
+}
+
+func ThreadedTraversal(t *ThreadedTree) {
+	// seek leftmost
+	for t.llink != nil {
+		t = t.llink
+	}
+	for t != nil && !t.isHead() {
+		fmt.Printf("%s ", t.info)
+		if t.rtag {
+			t = t.rlink
+		} else {
+			// seek leftmost of right link
+			t = t.rlink
+			for t.llink != nil {
+				t = t.llink
+			}
+		}
+	}
+
+}
+
+func (t *ThreadedTree) isHead() bool {
+	// By convention, HEAD.RLINK = HEAD and HEAD.RTAG = 0
+	return !t.rtag && t.rlink == t
+}
+
+func (t *Tree) InsertRight(p *Tree) {
+	if t.rlink == nil {
+		t.rlink = p
+	} else {
+		rlink := t.rlink
+		t.rlink = p
+		p.rlink = rlink
+	}
+}
+
+func (t *Tree) InsertLeft(p *Tree) {
+	if t.llink == nil {
+		t.rlink = p
+	} else {
+		llink := t.llink
+		t.llink = p
+		p.rlink = llink
+	}
 }
